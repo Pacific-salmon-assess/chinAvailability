ecc1c76ac8973aac57a9d28562267e1f5e893955
Cameron Freshwater <ccfreshwater@gmail.com>
2023-06-27 14:47:14 -0700

    add age comp exploratory figs

--- R/ctc_stock_comp.R
+++ R/ctc_stock_comp.R
@@ -8,11 +8,11 @@
 library(tidyverse)
 
 fishery_lookup <- read.csv(
-  here::here("data", "ctc", "2022ERA_FisheryLookup.csv")
+  here::here("data", "ctc", "fisheryCodes.csv")
 ) %>% 
   janitor::clean_names()
-stock_dat <- read.csv(
-  here::here("data", "ctc", "CWTDataforMapping_CTC_big.csv")
+stock_lookup <- read.csv(
+  here::here("data", "ctc", "stockCodes.csv")
 ) %>% 
   janitor::clean_names()
 isbm <- read.csv(
@@ -28,26 +28,72 @@ aabm <- read.csv(
 ## identify relevant fisheries
 fishery1 <- fishery_lookup %>% 
   filter(
-    fishery_name %in% c("WCVI F/W T", "WCVI SPR T", "WCVI SUM T", "WCVI AABM S",
-                        "WCVI ISBM S", "BC JF S", "TBC JF TERM S")
+    fishery_short_name %in% c(
+      "WCVI T", "GEO ST T", "WCVI AABM S", "WCVI ISBM S", "GEO ST S", "BC JF S"
+    )
   )
 
-# filter and calculate total catch, by age, across types 
+
+# filter and calculate total catch, by age, by fisheries/stock
 subset_catch <- rbind(isbm, aabm) %>% 
-  filter(fishery %in% fishery1$fishery_number) %>% 
+  filter(fishery %in% fishery1$fishery) %>% 
   left_join(., 
-            fishery_lookup %>% select(fishery = fishery_number, fishery_name),
-            by = "fishery") 
-sum_catch <- subset_catch %>% 
+            fishery_lookup %>% select(fishery, fishery_short_name),
+            by = "fishery") %>% 
+  mutate(age = as.factor(age))
+total_catch_at_age <- subset_catch %>% 
   select(starts_with("term") | starts_with("preterm")) %>% 
   apply(., 1, sum)
-subset_catch$sum_catch <- sum_catch
+subset_catch$total_catch_at_age <- total_catch_at_age
 
-subset_catch  %>% 
-  group_by(year, fishery, fishery_name) %>% 
-  summarize(sum_catch = sum(total_catch)) %>% 
-  ggplot() +
-  geom_jitter(aes(x = year, y =  fishery_name, size = sum_catch),
-              alpha = 0.5, width = 0.25) +
+
+# calculate total catch per fishery (regardless of age/stock)
+total_catch <- subset_catch %>% 
+  group_by(year, fishery, fishery_short_name) %>% 
+  summarize(total_catch = sum(total_catch_at_age), .groups = "drop") %>% 
+  ungroup()
+
+
+# calculate total catch at age per fishery (pooling stocks)
+subset_agg_catch <- subset_catch %>% 
+  group_by(year, age, fishery, fishery_short_name) %>% 
+  summarize(total_catch_at_age = sum(total_catch_at_age), .groups = "drop") %>% 
+  ungroup() %>% 
+  left_join(., total_catch, by = c("year", "fishery", "fishery_short_name")) %>% 
+  mutate(ppn_catch_at_age = total_catch_at_age / total_catch) 
+
+
+# age composition by fishery
+age_comp_bar <- ggplot(subset_agg_catch, 
+                       aes(fill = age, y = ppn_catch_at_age, x = year)) + 
+  geom_bar(position="stack", stat="identity") +
+  facet_wrap(~fishery_short_name) +
+  ggsidekick::theme_sleek()
+
+
+# visualize total catch by fisheries
+fishery_catch <- ggplot(total_catch) +
+  geom_jitter(aes(x = year, y =  fishery_short_name, size = total_catch),
+              alpha = 0.5, height = 0.05) +
   ggsidekick::theme_sleek() 
+
+
+# visualize average since 2014
+mean_ppn_bar <- subset_agg_catch %>% 
+  filter(year >= 2014, 
+         !fishery_short_name == "GEO ST T") %>% 
+  group_by(fishery, fishery_short_name, age) %>% 
+  summarize(mean_ppn = mean(ppn_catch_at_age)) %>% 
+  ggplot(., 
+         aes(fill = age, y = mean_ppn, x = fishery_short_name)) + 
+  geom_bar(position = "stack", stat = "identity") +
+  labs(x =  "Fishery", y = "Proportion of Catch (since 2014)") +
+  ggsidekick::theme_sleek()
+
+
+pdf(here::here("figs", "fishery_age_comp", "age_comp_exp.pdf"))
+fishery_catch
+age_comp_bar
+mean_ppn_bar
+dev.off()
 
