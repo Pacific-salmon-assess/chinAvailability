ee6daa167ae4570d7473f0125e279e090f155fe5
CamFreshwater <ccfreshwater@gmail.com>
2024-04-14 19:55:22 -0700

    keep tweaking stock size ests

--- R/size_by_stock.R
+++ R/size_by_stock.R
@@ -8,15 +8,16 @@ library(tidyverse)
 
 gsi <- readRDS(here::here("data", "rec", "rec_gsi.rds")) %>% 
   mutate(
+    #add management measure effect accounting for slot limits
+    mm = ifelse(year %in% c("2019", "2020", "2021", "2022"), "yes", "no"),
     stock_group = case_when(
+      stock == "Capilano" | region1name %in% c("ECVI", "SOMN") ~ "ECVI_SOMN",
       grepl("Fraser", region1name) ~ region1name,
       TRUE ~ pst_agg
-    ),
-    #add management measure effect accounting for slot limits
-    mm = ifelse(year %in% c("2019", "2020", "2021", "2022"), "yes", "no")
-  ) %>% 
+    )
+  ) %>%
   group_by(
-    id, stock_group, week_n, month_n,  mm, fl, age, year
+    id, strata, stock_group, week_n, month_n,  mm, fl, age, year
   ) %>% 
   summarize(
     sum_prob = sum(prob), .groups = "drop"
@@ -26,34 +27,39 @@ gsi <- readRDS(here::here("data", "rec", "rec_gsi.rds")) %>%
   # remove uncertain assignments
   filter(
     sum_prob < 0.75,
-    # !cap_region == "outside",
-    month_n > 4 & month_n < 11,
-    stock_group %in% c(
-      "CR-upper_sp", "CR-lower_sp", "CR-upper_su/fa",
-      "CR-lower_fa", "WCVI", "Fraser_Spring_5.2", "Fraser_Spring_4.2",
-      "Fraser_Summer_5.2",  "Fraser_Summer_4.1", "Fraser_Fall", "SOG", "PSD"
-    )
+    !strata == "other"
   ) %>% 
   mutate(
-    stock_group = fct_relevel(
-      as.factor(stock_group), "CR-upper_sp", "CR-lower_sp", "CR-upper_su/fa",
-      "CR-lower_fa", "WCVI", "Fraser_Spring_5.2", "Fraser_Spring_4.2",
-      "Fraser_Summer_5.2",  "Fraser_Summer_4.1", "Fraser_Fall", "SOG", "PSD"
-    )
+    strata = factor(
+      strata,
+      levels = c("swiftsure", "swiftsure_nearshore", "renfrew", "vic",
+                 "haro", "saanich"),
+      labels = c("Swiftsure", "Nitinat", "Renfrew", "Sooke/\nVictoria",
+                 "S. Gulf\nIslands", "Saanich")
+    ),
+    stock_group = factor(
+      stock_group,
+      levels = c(
+        "CA_ORCST", "CR-upper_sp", "CR-upper_su/fa", "CR-lower_sp",
+        "CR-lower_fa", "WACST", "PSD", "WCVI", "ECVI_SOMN",
+        "Fraser_Spring_4.2", "Fraser_Spring_5.2", "Fraser_Summer_5.2",
+        "Fraser_Summer_4.1", "Fraser_Fall",  "NBC_SEAK"
+      )
+    ),
+    year_f = as.factor(year)
   )
   
 
 ## AGE COMPOSITION -------------------------------------------------------------
 
 age_comp <- gsi %>%
-  filter(!is.na(age)) %>% 
+  filter(!is.na(age_gr)) %>% 
   group_by(stock_group) %>%
   mutate(age_n = n()) %>%
   ungroup() %>%
-  group_by(stock_group, age, age_n) %>%
+  group_by(stock_group, age_gr, age_n) %>%
   tally() %>% 
-  mutate(prop = n / age_n,
-         age = as.factor(age))
+  mutate(prop = n / age_n)
 
 labs_age_comp <- age_comp %>%
   ungroup() %>% 
@@ -62,23 +68,33 @@ labs_age_comp <- age_comp %>%
 
 age_comp_stacked <- ggplot() +
   geom_bar(data = age_comp,
-           aes(fill = age, y = prop, x = stock_group),
+           aes(fill = as.factor(age_gr), y = prop, x = stock_group),
            position="stack", stat="identity") +
   geom_text(data = labs_age_comp, 
             aes(x = stock_group, y = 0.05, label = age_n)) +
-  scale_fill_viridis_d(name = "Age", na.value = "grey60" ) +
+  scale_fill_brewer(name = "Age", palette = "Paired", na.value = "grey60" ) +
+  
+  # scale_fill_viridis_d(name = "Age", na.value = "grey60" ) +
   labs(y = "Proportion Age Composition", x = "Stock") +
   ggsidekick::theme_sleek() +
   theme(
     axis.text.x = element_text(angle = 45, hjust=1)
   )
 
+png(
+  here::here("figs", "ms_figs", "comp_bar_fishery_age.png"),
+  height = 5, width = 8, units = "in", res = 250
+)
+age_comp_stacked
+dev.off()
+
 
 ## BODY SIZE -------------------------------------------------------------------
 
 size_at_age_box <- gsi %>%
-  filter(!is.na(age),
-         !age %in% c("2", "6")) %>% 
+  filter(!is.na(age)#,
+         # !age %in% c("2", "6")
+         ) %>% 
   ggplot(.) +
   geom_boxplot(aes(x = stock_group, y = fl)) +
   labs(y = "Fork Length", x = "Stock") +
@@ -97,23 +113,20 @@ ggplot(gsi %>% filter(fl > 550)) +
   )
 
 
-## significant differences among stocks?
-size_trim <- gsi %>% 
-  filter(fl > 550) %>% 
-  mutate(year_f = as.factor(year))
-
 
 library(mgcv)
 
 fit2 <- gam(
-  fl ~ s(week_n, bs = "tp", k = 4, m = 2) +
+  fl ~ 0 + age + s(week_n, bs = "tp", k = 4, m = 2) +
     s(week_n, bs = "tp", by = stock_group, k = 4, m = 1) +
     mm:stock_group + s(year_f, bs = "re"),
-  data = size_trim
+  data = gsi
 )
+
 
 # make predictions, constraining to weeks where stocks present
 stock_week <- gsi %>% 
+  filter(month_n > 4 & month_n < 11) %>% 
   group_by(week_n, stock_group) %>% 
   tally() 
 
@@ -136,7 +149,8 @@ week_month <- data.frame(
 )
 new_dat <- expand.grid(
   week_n = unique(week_month$week_n),
-  stock_group = unique(size_trim$stock_group)
+  stock_group = unique(gsi$stock_group),
+  age = levels(gsi$age)
 ) %>% 
   left_join(., week_month, by = "week_n") %>% 
   left_join(., obs_weeks, by = "stock_group") %>% 
@@ -159,22 +173,10 @@ new_dat2 <- new_dat %>%
     up_fl = pred_fl + (stats::qnorm(0.975) * as.numeric(preds$se.fit))
     )
 
-size_month1 <- ggplot(new_dat2) +
-  geom_pointrange(
-    aes(x = stock_group, y = pred_fl, ymin = lo_fl, ymax = up_fl, 
-        fill = stock_group),
-    shape = 21
-  ) +
-  facet_wrap(~month) +
-  ggsidekick::theme_sleek() +
-  theme(
-    axis.text.x = element_blank()
-  )
-
 size_month2 <- ggplot(new_dat2) +
   geom_pointrange(
     aes(x = month, y = pred_fl, ymin = lo_fl, ymax = up_fl, 
-        fill = stock_group),
+        fill = age),
     shape = 21
   ) +
   facet_wrap(~stock_group) +
@@ -182,6 +184,13 @@ size_month2 <- ggplot(new_dat2) +
   theme(
     axis.title.x = element_blank()
   )
+
+
+png(
+  here::here("figs", "ms_figs", "temporal_sample_coverage.png"),
+  height = 5, width = 7.5, units = "in", res = 250
+)
+
 
 
 pdf(here::here("figs", "stock_size_age", "summary_figs.pdf"))
