27c80f40b181223791bfd6ec0ccd5f4e36bb0802
CamFreshwater <ccfreshwater@gmail.com>
2024-05-25 13:47:37 +0100

    update custom pred function to accept non-tweedie families

--- R/functions/pred_mvtweedie2.R
+++ R/functions/pred_mvtweedie2.R
@@ -1,3 +1,7 @@
+# modified version predict.mvtweedie that a) removes VAST architecture, 
+# b) incorporates "exclude" argument from predict.mgcv, c) allows non-tweedie
+# families (should be restricted to Poisson and Negative Binomial)
+
 pred_dummy <- function( x,
             #                  original_class = "glmmTMB",
             category_name = "group",
@@ -6,14 +10,14 @@ pred_dummy <- function( x,
             se.fit = FALSE,
             exclude = NULL) {
     # Error checks
-    if( any(c("gam","glmmTMB") %in% class(x)) ){
-      if( tolower(substr(family(x)$family,1,7)) != "tweedie" ) warning("`predict.mvtweedie` only implemented for a Tweedie distribution")
-      if( family(x)$link != "log" ) stop("`predict.mvtweedie` only implemented for a log link")
-    }else if( "fit_model"%in%class(x) ){
-      if( se.fit==TRUE ) error("se.fit not implemented for predict using VAST")
-    }else{
-      stop("`predict.mvtweedie` only implemented for mgcv, glmmTMB and VAST")
-    }
+    # if( any(c("gam","glmmTMB") %in% class(x)) ){
+    #   if( tolower(substr(family(x)$family,1,7)) != "tweedie" ) warning("`predict.mvtweedie` only implemented for a Tweedie distribution")
+    #   if( family(x)$link != "log" ) stop("`predict.mvtweedie` only implemented for a log link")
+    # }else if( "fit_model"%in%class(x) ){
+    #   if( se.fit==TRUE ) error("se.fit not implemented for predict using VAST")
+    # }else{
+    #   stop("`predict.mvtweedie` only implemented for mgcv, glmmTMB and VAST")
+    # }
     
     # Check and account for tibbles
     if( "package:tibble" %in% search() ){
@@ -27,7 +31,10 @@ pred_dummy <- function( x,
     if(missing(newdata) || is.null(newdata)) newdata = origdata
     
     # Predict each observation for each class
-    se_pred_ic = pred_ic = array(NA, dim=c(nrow(newdata),nlevels(origdata[,category_name])))
+    se_pred_ic = pred_ic = array(
+      NA, 
+      dim = c(nrow(newdata), nlevels(origdata[,category_name]))
+      )
     for(cI in 1:ncol(pred_ic)){
       
       # Modify data
@@ -37,7 +44,7 @@ pred_dummy <- function( x,
         levels=levels(origdata[,category_name]) 
         )
       
-      # include hacky version
+      # include hacky version to account for year-group specific RIs
       if ("sg_year" %in% colnames(data)) {
         data$sg_year <- paste(
           factor(levels(origdata[,category_name])[cI], 
@@ -49,21 +56,21 @@ pred_dummy <- function( x,
       }
       
       # Modify class
-      class(x) = setdiff( class(x), "mvtweedie" )
+      # class(x) = setdiff( class(x), "mvtweedie" )
       #class(x) = original_class
       
       # Apply predict.original_class
-      if( "fit_model" %in% class(x) ){
+      # if( "fit_model" %in% class(x) ){
         # if using VAST
-        pred_ic[,cI] = predict(x,
-                               what="D_i",
-                               Lat_i=x$data_frame[,'Lat_i'],
-                               Lon_i=x$data_frame[,'Lon_i'],
-                               t_i=x$data_frame[,'t_i'],
-                               a_i=x$data_frame[,'a_i'],
-                               c_iz=rep(cI-1,nrow(x$data_frame)),
-                               v_i=x$data_frame[,'v_i'] )
-      }else{
+        # pred_ic[,cI] = predict(x,
+      #                          what="D_i",
+      #                          Lat_i=x$data_frame[,'Lat_i'],
+      #                          Lon_i=x$data_frame[,'Lon_i'],
+      #                          t_i=x$data_frame[,'t_i'],
+      #                          a_i=x$data_frame[,'a_i'],
+      #                          c_iz=rep(cI-1,nrow(x$data_frame)),
+      #                          v_i=x$data_frame[,'v_i'] )
+      # }else{
         pred = predict(x,
                        newdata = data,
                        type="response",
@@ -82,7 +89,7 @@ pred_dummy <- function( x,
           pred_ic[,cI] = pred
         }
       }
-    }
+    # }
     
     # Normalize probability for each observation and class
     rowsum_pred_ic = outer( rowSums(pred_ic), rep(1,ncol(pred_ic)) )
