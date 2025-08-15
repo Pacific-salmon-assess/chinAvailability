ad37e238788bf1bcb4c086cee6c0fed47b675de8
Freshwater <Cameron.Freshwater@dfo-mpo.gc.ca>
2025-08-15 11:11:45 -0700

    move sep plot data out of gitignore

--- R/sep_figs.R
+++ R/sep_figs.R
@@ -29,74 +29,83 @@ fig_path <- function(filename) {
 library(tidyverse)
 
 
-pbt_rate <- readRDS(here::here("data", "sep", "cleaned_pbt.rds"))
+# pbt_rate <- readRDS(here::here("data", "sep", "cleaned_pbt.rds"))
 long_rec <- readRDS(here::here("data", "rec", "rec_gsi.rds"))
 
 
 # import SEP release estimates to identify major stocks (defined as facilities
 # w/ greater than 140k average releases per year, following Brock's advice)
-sep_rel <- readxl::read_xlsx(
-  here::here("data", "sep", "2018-2022BY avg release.xlsx")
-) %>% 
-  janitor::clean_names() %>% 
-  filter(
-    avg_rel > 140000 | stock_name %in% c("Nechako R", "Chilko R")
-  ) %>% 
-  mutate(
-    #rename to match pbt_rate df
-    stock_name = case_when(
-      stock_name == "Big Qualicum R" ~ "Qualicum River",
-      stock_name == "L Qualicum R" ~ "Little Qualicum River",
-      stock_name == "Shuswap R Low" ~ "Shuswap River Lower",
-      stock_name == "Shuswap R Middle" ~ "Shuswap River Middle",
-      stock_name == "Chehalis R" ~ "Chehalis River Summer",
-      stock_name == "Kitsumkalum R" ~ "Kitsumkalum River Lower",
-      stock_name == "Chilliwack R" ~ "Chilliwack River Fall",
-      stock_name == "Nanaimo R" ~ "Nanaimo River Fall",
-      grepl(" R", stock_name) ~ paste(stock_name, "iver", sep = ""),
-      grepl(" Cr", stock_name) ~ paste(stock_name, "eek", sep = ""),
-      TRUE ~ stock_name
-    ) %>% 
-      toupper(),
-    pbt_stock = gsub(" ", "_", stock_name)
+# sep_rel <- readxl::read_xlsx(
+#   here::here("data", "sep", "2018-2022BY avg release.xlsx")
+# ) %>% 
+#   janitor::clean_names() %>% 
+#   filter(
+#     avg_rel > 140000 | stock_name %in% c("Nechako R", "Chilko R")
+#   ) %>% 
+#   mutate(
+#     #rename to match pbt_rate df
+#     stock_name = case_when(
+#       stock_name == "Big Qualicum R" ~ "Qualicum River",
+#       stock_name == "L Qualicum R" ~ "Little Qualicum River",
+#       stock_name == "Shuswap R Low" ~ "Shuswap River Lower",
+#       stock_name == "Shuswap R Middle" ~ "Shuswap River Middle",
+#       stock_name == "Chehalis R" ~ "Chehalis River Summer",
+#       stock_name == "Kitsumkalum R" ~ "Kitsumkalum River Lower",
+#       stock_name == "Chilliwack R" ~ "Chilliwack River Fall",
+#       stock_name == "Nanaimo R" ~ "Nanaimo River Fall",
+#       grepl(" R", stock_name) ~ paste(stock_name, "iver", sep = ""),
+#       grepl(" Cr", stock_name) ~ paste(stock_name, "eek", sep = ""),
+#       TRUE ~ stock_name
+#     ) %>% 
+#       toupper(),
+#     pbt_stock = gsub(" ", "_", stock_name)
+#   )
+
+
+# pbt_rate_plot <- pbt_rate %>% 
+#   mutate(
+#     brood_year = as.factor(brood_year),
+#     stock_group = case_when(
+#       pbt_stock %in% 
+#         c("CAPILANO_RIVER", "COWICHAN_RIVER", "LITTLE_QUALICUM_RIVER",
+#           "NANAIMO_RIVER_FALL", "QUALICUM_RIVER", "PUNTLEDGE_RIVER") ~ "ECVI_SOMN",
+#       pbt_stock %in% c("CHEHALIS_RIVER_SUMMER", "CHILLIWACK_RIVER_FALL",
+#                        "HARRISON_RIVER") ~ "FR_Fall",
+#       pbt_stock %in% c("SHUSWAP_RIVER_LOWER", "SHUSWAP_RIVER_MIDDLE") ~ 
+#         "FR_Sum_4sub1",
+#       pbt_stock %in% c("CHILKO_RIVER", "NECHAKO_RIVER") ~ "FR_Sum_5sub2",
+#       pbt_stock == "NICOLA_RIVER" ~ "FR_Spr_4sub2",
+#       TRUE ~ "WCVI"
+#     ) %>% 
+#       factor(., levels = levels(long_rec$stock_group)),
+#     pbt_stock = fct_reorder(pbt_stock, as.numeric(stock_group))
+#   ) %>% 
+#   # focus on large facilities
+#   filter(
+#     pbt_stock %in% sep_rel$pbt_stock,
+#     # remove northern stocks 
+#     !pbt_stock %in% c("ATNARKO_RIVER", "BURMAN_RIVER", 
+#                       "GOLD_RIVER", "KITIMAT_RIVER", "KITSUMKALUM_RIVER_LOWER",
+#                       "LANG_CREEK", "LEINER_RIVER", "MARBLE_RIVER", 
+#                       "NIMPKISH_RIVER", "QUINSAM_RIVER", "TAHSIS_RIVER", 
+#                       "WANNOCK_RIVER", "WOSS_RIVER", "YAKOUN_RIVER")
+#   )
+# saveRDS(
+#   pbt_rate_plot,
+#   here::here(
+#     "data", "sep_plot_data.rds"
+#   )
+# )
+pbt_rate_plot <- readRDS(
+  here::here(
+    "data", "sep_plot_data.rds"
   )
-
-
+)
 
 smu_colour_pal <- c("grey30", "#3182bd", "#bdd7e7", "#bae4bc", "#6A51A3",
                     "#CBC9E2", "#67000D", "#A50F15", "#EF3B2C", "#FC9272", 
                     "#FCBBA1")
 names(smu_colour_pal) <- levels(long_rec$stock_group)
-
-
-pbt_rate_plot <- pbt_rate %>% 
-  mutate(
-    brood_year = as.factor(brood_year),
-    stock_group = case_when(
-      pbt_stock %in% 
-        c("CAPILANO_RIVER", "COWICHAN_RIVER", "LITTLE_QUALICUM_RIVER",
-          "NANAIMO_RIVER_FALL", "QUALICUM_RIVER", "PUNTLEDGE_RIVER") ~ "ECVI_SOMN",
-      pbt_stock %in% c("CHEHALIS_RIVER_SUMMER", "CHILLIWACK_RIVER_FALL",
-                       "HARRISON_RIVER") ~ "FR_Fall",
-      pbt_stock %in% c("SHUSWAP_RIVER_LOWER", "SHUSWAP_RIVER_MIDDLE") ~ 
-        "FR_Sum_4sub1",
-      pbt_stock %in% c("CHILKO_RIVER", "NECHAKO_RIVER") ~ "FR_Sum_5sub2",
-      pbt_stock == "NICOLA_RIVER" ~ "FR_Spr_4sub2",
-      TRUE ~ "WCVI"
-    ) %>% 
-      factor(., levels = levels(long_rec$stock_group)),
-    pbt_stock = fct_reorder(pbt_stock, as.numeric(stock_group))
-  ) %>% 
-  # focus on large facilities
-  filter(
-    pbt_stock %in% sep_rel$pbt_stock,
-    # remove northern stocks 
-    !pbt_stock %in% c("ATNARKO_RIVER", "BURMAN_RIVER", 
-                      "GOLD_RIVER", "KITIMAT_RIVER", "KITSUMKALUM_RIVER_LOWER",
-                      "LANG_CREEK", "LEINER_RIVER", "MARBLE_RIVER", 
-                      "NIMPKISH_RIVER", "QUINSAM_RIVER", "TAHSIS_RIVER", 
-                      "WANNOCK_RIVER", "WOSS_RIVER", "YAKOUN_RIVER")
-  )
 
 
 pbt_coverage <- ggplot(pbt_rate_plot, aes(x = brood_year, y = tag_rate)) +
@@ -125,35 +134,46 @@ dev.off()
 
 # use SEP data to fit simple LM to estimate mean hatchery contribution for WCVI 
 # and ECVI systems
-phos <- readxl::read_xlsx(
-  here::here("data", "sep", "2024-08-20 annual pHOS summary.xlsx")
-) %>% 
-  filter(
-    estimate_type %in% c("Direct", "Partial Direct"),
-    cu_acronym %in% c("SWVI", "Chil_transp_F", "QP-fall", "MFR-summer",
-                      "CWCH-KOK", "LFR-fall", "EVI-fall", "EVIGStr-sum",
-                      "LTh", "NEVI", "STh-1.3", "STh-SHUR", "NEVI")
-  ) %>% 
-  mutate(
-    stock_group = case_when(
-      cu_acronym == "SWVI" ~ "WCVI",
-      cu_acronym %in% c("Chil_transp_F", "LFR-fall") ~ "FR_Fall",
-      grepl("EVI", cu_acronym) | cu_acronym %in% c("CWCH-KOK", "QP-fall") ~ 
-        "ECVI_SOMN",
-      cu_acronym == "MFR-summer" ~ "FR_Sum_5sub2",
-      cu_acronym == "LTh" ~ "FR_Spr_4sub2",
-      cu_acronym == "STh-1.3" ~ "FR_Spr_5sub2",
-      cu_acronym == "STh-SHUR" ~ "FR_Sum_4sub1"
-    ),
-    stock_group = factor(stock_group, levels = levels(long_rec$stock_group)),
-    population = gsub(" River", "", population),
-    population = gsub(" Creek", "", population),
-    population = fct_reorder(population, as.numeric(stock_group)),
-    pHOS = ifelse(pHOS == "1", 0.999, pHOS),
-    pHOS = ifelse(pHOS == "0", 0.001, pHOS)
-  ) 
+# phos_plot_dat <- readxl::read_xlsx(
+#   here::here("data", "sep", "2024-08-20 annual pHOS summary.xlsx")
+# ) %>% 
+#   filter(
+#     estimate_type %in% c("Direct", "Partial Direct"),
+#     cu_acronym %in% c("SWVI", "Chil_transp_F", "QP-fall", "MFR-summer",
+#                       "CWCH-KOK", "LFR-fall", "EVI-fall", "EVIGStr-sum",
+#                       "LTh", "NEVI", "STh-1.3", "STh-SHUR", "NEVI")
+#   ) %>% 
+#   mutate(
+#     stock_group = case_when(
+#       cu_acronym == "SWVI" ~ "WCVI",
+#       cu_acronym %in% c("Chil_transp_F", "LFR-fall") ~ "FR_Fall",
+#       grepl("EVI", cu_acronym) | cu_acronym %in% c("CWCH-KOK", "QP-fall") ~ 
+#         "ECVI_SOMN",
+#       cu_acronym == "MFR-summer" ~ "FR_Sum_5sub2",
+#       cu_acronym == "LTh" ~ "FR_Spr_4sub2",
+#       cu_acronym == "STh-1.3" ~ "FR_Spr_5sub2",
+#       cu_acronym == "STh-SHUR" ~ "FR_Sum_4sub1"
+#     ),
+#     stock_group = factor(stock_group, levels = levels(long_rec$stock_group)),
+#     population = gsub(" River", "", population),
+#     population = gsub(" Creek", "", population),
+#     population = fct_reorder(population, as.numeric(stock_group)),
+#     pHOS = ifelse(pHOS == "1", 0.999, pHOS),
+#     pHOS = ifelse(pHOS == "0", 0.001, pHOS)
+#   ) 
+# saveRDS(
+#   phos_plot_dat,
+#   here::here(
+#     "data", "phos_plot_data.rds"
+#   )
+# )
+phos_plot_dat <- readRDS(
+  here::here(
+    "data", "phos_plot_data.rds"
+  )
+)
 
-phos_box <- ggplot(phos) +
+phos_box <- ggplot(phos_plot_dat) +
   geom_boxplot(aes(x = population, y = pHOS, fill = stock_group)) +
   scale_fill_manual(values = smu_colour_pal) +
   labs(y = tr("Proportion Hatchery Origin Spawners", "Proportion de géniteurs d'origine d'écloserie"), fill = NULL) +
